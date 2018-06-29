ddae93ef0c348af59a25f08e62333055188b7345
haseungwon <hapeter@naver.com>
2018-06-29 16:42:20 +0900

    메시지 url 설정과 선택1 답변 설정

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 def keyboard(request):
         return JsonResponse(
             {
@@ -9,3 +10,19 @@ def keyboard(request):
                 "buttons" : ["선택 1", "선택 2", "선택 3"]
             }
         )
+def message(request):
+    return JsonResponse(
+        message = ((request.body).decode('utf-8'))
+        return_json_str = json.loads(message)
+        return_str = return_json_str['content']
+
+        if return_str =="선택 1":
+            return JsonResponse(
+                "message": 
+                {
+                    "text": "안녕하십니까~~"
+
+                }
+            )
+
+    )
