f6296718ead85ac5016ddc254dc7e92730007cc6
haseungwon <hapeter@naver.com>
2018-06-29 17:58:34 +0900

    날씨 수정 6

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -46,14 +46,14 @@ def message(request):
         }
         )
 def weather(response):
-        params = {"version": "1", "city":"서울", "county":"광진구","village":"군자동"}
-        headers = {"appKey": "a88bad75-0102-430d-adc9-1d555674a640"}
-        response = requests.get("https://api2.sktelecom.com/weather/current/minutely", params=params, headers=headers)
-        data = json.loads(response.text)
-        weather = data["weather"]["minutely"]
-        sky = weather[0]["sky"]["name"]
-        wind = weather[0]["wind"]["wspd"]
-        temp = weather[0]["temperature"]["tc"]
-        time = weather[0]["timeObservation"]
-        printweather = '하늘 : ' + sky + '\n' + '온도 : ' + temp + 'C\n' + '풍속 : ' + wind + 'm/s'
-        return printweather
+    params = {"version": "1", "city":"서울", "county":"광진구","village":"군자동"}
+    headers = {"appKey": "a88bad75-0102-430d-adc9-1d555674a640"}
+    response = requests.get("https://api2.sktelecom.com/weather/current/minutely", params=params, headers=headers)
+    data = json.loads(response.text)
+    weather = data["weather"]["minutely"]
+    sky = weather[0]["sky"]["name"]
+    wind = weather[0]["wind"]["wspd"]
+    temp = weather[0]["temperature"]["tc"]
+    time = weather[0]["timeObservation"]
+    printweather = '하늘 : ' + sky + '\n' + '온도 : ' + temp + 'C\n' + '풍속 : ' + wind + 'm/s'
+    return printweather
