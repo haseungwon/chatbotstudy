d613274098a3d8a9386969e8e330afab453b1343
haseungwon <hapeter@naver.com>
2018-06-29 16:03:59 +0900

    키보드 설정

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decoratoirs.csrf import csrf_exempt
+def keyboard(request):
+        return JsonResponse(
+            {
+                "type" : "buttons",
+                "buttons" : ["선택 1", "선택 2", "선택 3"]
+            }
+        )
